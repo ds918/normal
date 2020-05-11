effe291543c9c192d890c426922129a64995e63f
sennorwell <51373319+sennorwell@users.noreply.github.com>
2020-05-11 22:01:54 +0800

    Update Index.vue

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -7,15 +7,16 @@
 export default {
   name: "index",
   async activated() {
-    await this.$http
-      .post({
-        url: "NewSpecialBreakWrongCollectionExercise",
-        data:
-          "offset_number=0&course_id=1&user_id=165640&token=4e87b5765ac1334abc791eeb7cbf6e14&take_number=1000"
-      })
-      .then(res => {
-        console.log(res);
-      });
+    let data = await this.$http.post({
+      url: "NewSpecialBreakWrongCollectionExercise",
+      data:
+        "offset_number=0&course_id=1&user_id=165640&token=4e87b5765ac1334abc791eeb7cbf6e14&take_number=1000"
+    });
+    console.log(data);
+
+    // .then(res => {
+    //   console.log(res);
+    // });
   }
 };
 </script>
