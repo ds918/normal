e03bf1626a8e151fdd6a98e33adb513c6276466d
dongsen <1415222054@qq.com>
2020-05-21 10:18:32 +0800

    check

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,18 +1,17 @@
 import Vue from "vue";
 import Vuetify from "vuetify/lib/framework";
-import '@/styles/index.scss';
+import "@/styles/index.scss";
 
 Vue.use(Vuetify);
 
-import zhHans from 'vuetify/es5/locale/zh-Hans'
+import zhHans from "vuetify/es5/locale/zh-Hans";
 
 export default new Vuetify({
   breakpoint: {
-    scrollBarWidth: 300
+    scrollBarWidth: 300,
   },
   lang: {
     locales: { zhHans },
-    current: 'zhHans',
+    current: "zhHans",
   },
-
 });
