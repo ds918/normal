4c6f92980672764aa7928514ff64504cdc7a9a64
dongsen <1415222054@qq.com>
2020-06-01 14:14:59 +0800

    latest

--- src/components/bottom-navigation.vue
+++ src/components/bottom-navigation.vue
@@ -8,15 +8,15 @@
       background-color="#fff"
       v-model="NAVIGATION_INDEX"
     >
-      <v-btn height="100%" min-width="33.33333%" to="/" value="0">
+      <v-btn height="100%" min-width="33.33333%" to value="0">
         <span>Recents</span>
         <v-icon :size="$V(34)">mdi-history</v-icon>
       </v-btn>
-      <v-btn height="100%" min-width="33.33333%" to="/test" value="1">
+      <v-btn height="100%" min-width="33.33333%" to value="1">
         <span>Favorites</span>
         <v-icon :size="$V(34)">mdi-heart</v-icon>
       </v-btn>
-      <v-btn height="100%" min-width="33.33333%" to="/test1" value="2">
+      <v-btn height="100%" min-width="33.33333%" to value="2">
         <span>Nearby</span>
         <v-icon :size="$V(34)">mdi-map-marker</v-icon>
       </v-btn>
@@ -33,9 +33,9 @@ export default {
       },
       set(index) {
         this.$store.commit("navigation/NAVIGATION_TARGET", index);
-      },
-    },
-  },
+      }
+    }
+  }
 };
 </script>
 <style lang="scss" module>
